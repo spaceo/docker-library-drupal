a7d2e7cf0d51b482e2f81983a5f7e3ed7b149b5a
Mikkel Jakobsen <mikkel@adapt.dk>
2015-10-07 23:34:48 +0200

    Adding xhprof and php.ini for cli

---
+++ 8/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+  
+#make sure xdebug remote host is set
+# remove xdebug.remote_host line from xdebug.ini
+sed -i '/xdebug.remote_host/d' /usr/local/etc/php/conf.d/xdebug.ini
+
+# gateway == remote host for debugging
+gateway=`route |grep default |awk '{print $2}'`
+
+(
+cat << EOF1
+xdebug.remote_host = $gateway
+EOF1
+) >> /usr/local/etc/php/conf.d/xdebug.ini
+
+# Reload apache2 to adjust to xdebug settings.
+service apache2 reload
+
+# Start syslog
+service rsyslog start
