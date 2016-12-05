c6b9f96f8d08390978914a95f70837e35ef2472f
Mikkel Jakobsen <mikkel@adapt.dk>
2016-12-05 12:14:33 +0100

    Reverting dockerfile and start to even earlier stage to see if it fixes osx error

--- 7/start.sh
+++ 7/start.sh
@@ -2,16 +2,16 @@
   
 #make sure xdebug remote host is set
 # remove xdebug.remote_host line from xdebug.ini
-#sed -i '/xdebug.remote_host/d' /usr/local/etc/php/conf.d/xdebug.ini
+sed -i '/xdebug.remote_host/d' /usr/local/etc/php/conf.d/xdebug.ini
 
 # gateway == remote host for debugging
-#gateway=`route |grep default |awk '{print $2}'`
+gateway=`route |grep default |awk '{print $2}'`
 
-#(
-#cat << EOF1
-#xdebug.remote_host = $gateway
-#EOF1
-#) >> /usr/local/etc/php/conf.d/xdebug.ini
+(
+cat << EOF1
+xdebug.remote_host = $gateway
+EOF1
+) >> /usr/local/etc/php/conf.d/xdebug.ini
 
 # Reload apache2 to adjust to xdebug settings.
 service apache2 reload
